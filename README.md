ddce52ba84a40206135077a05beb7e311b71f7cd
abdelkarim <abdelkarim@uniris.io>
2019-05-13 14:47:08 +0200

    test auto approve

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ This application help Uniris Node to sign and Decrypt messages using a private k
 
 Run `make load` to build and load the application onto the device. After
 installing and running the application, you can run `make delete` to delete the app from the ledger device.
+
+To test the application just execute this: `python client.py data_to_sign`
+Example: python client.py 2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824
--- client.py
+++ client.py
@@ -2,23 +2,26 @@
 #*******************************************************************************
 #*   Made by Uniris
 #********************************************************************************
+import time
+import sys
 from ledgerblue.comm import getDongle
 from ledgerblue.commException import CommException
 from secp256k1 import PublicKey
 
-textToSign = ""
-while True:
-	data = raw_input("Enter text to sign, end with an empty line : ")
-	if len(data) == 0:
-		break
-	textToSign += data + "\n"
+
+
+textToSign = sys.argv[1]
 
 # To get the Public Key
+start = time.time()
 dongle = getDongle(True)
 publicKey = dongle.exchange(bytes("8004000000".decode('hex')))
+end = time.time()
 print "publicKey " + str(publicKey).encode('hex')
+print "Getting the public key takes:" , (end - start)
 
 try:
+	start = time.time()
 	offset = 0
 	while offset <> len(textToSign):
 		if (len(textToSign) - offset) > 255:
@@ -30,10 +33,12 @@ try:
 		else:
 			p1 = 0x00
 		#PDU size is limited, so we send the message here over many pdu and depending on the size of the data.
-		apdu = bytes("8002".decode('hex')) + chr(p1) + chr(0x00) + chr(len(chunk)) + bytes(chunk)
+		apdu = bytes("8008".decode('hex')) + chr(p1) + chr(0x00) + chr(len(chunk)) + bytes(chunk)
 		signature = dongle.exchange(apdu)
 		offset += len(chunk)
+	end = time.time()
 	print "signature " + str(signature).encode('hex')
+	print "Signing the message takes:" , (end - start)
 	publicKey = PublicKey(bytes(publicKey), raw=True)
 	signature = publicKey.ecdsa_deserialize(bytes(signature))
 	print "verified " + str(publicKey.ecdsa_verify(bytes(textToSign), signature))
